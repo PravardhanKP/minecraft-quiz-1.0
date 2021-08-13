3c15e063af34cede6b8ed6a524052c1cb0cb15d9
PravardhanKP <78488084+PravardhanKP@users.noreply.github.com>
2021-08-13 16:12:05 +0530

    Update code.js

--- code.js
+++ code.js
@@ -15,12 +15,12 @@ function Clues() {
     
    
     text("I can teleport,my home is the end,water damages me,I can lift blocks,I'm killed for my pearl by players.WHO AM I?", 760,340)
-
   
     textSize(15)
    
   
     text("Black skelitons are killed for their head.I'm spawned by 4 sole sand and 3 black skeliton heads,I have 300 harta and tough to kill me.WHO AM I?", 100,470)
+    
     text("Dream and many youtubers speedrun me.I leave in the end,I'm the strongest mob even if I have 200 harts.WHO AM I", 560,560)
 }
 
